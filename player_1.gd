5a84fe02911dc13cdc9269ef9410297a82aca3de
Aaron Schuessler <aaron@schuessler.vip>
2024-02-05 20:12:58 +0100

    added movement

--- player_1.gd
+++ player_1.gd
@@ -3,13 +3,27 @@ extends CharacterBody2D
 # import game
 const Game = preload("res://src/game.gd")
 # some consts
-const SPEED = 100
+const SPEED = 1000
 
 var main
 var game
 
+func read_user_input():
+	var move = Vector2i(0, 0)
+	if Input.is_key_pressed(KEY_W) || Input.is_key_pressed(KEY_UP) || Input.is_key_pressed(KEY_SPACE):
+		move.y -= 1
+	if Input.is_key_pressed(KEY_A) || Input.is_key_pressed(KEY_LEFT):
+		move.x -= 1
+	if Input.is_key_pressed(KEY_S) || Input.is_key_pressed(KEY_DOWN) || Input.is_key_pressed(KEY_SHIFT) || Input.is_key_pressed(KEY_Q):
+		move.y += 1
+	if Input.is_key_pressed(KEY_D) || Input.is_key_pressed(KEY_RIGHT):
+		move.x += 1
+	return move
+
 func _physics_process(delta):
 	game.update_platforms(delta)
+	var move = read_user_input()
+	game.move(move.x, move.y)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,3 +35,4 @@ func _ready():
 
 func _main_ready():
 	game.register_platform("res://Platforms/dirt3-1.tscn", 0, 0)
+	game.register_platform("res://Platforms/dirt3-1.tscn", -500, 20)
--- src/game.gd
+++ src/game.gd
@@ -4,6 +4,8 @@ var _platsforms: Array[Node2D]
 var _main
 var _speed
 
+var _velocity: Vector2
+
 func _init(main: Node2D, speed: float):
 	_main = main
 	_speed = speed
@@ -16,4 +18,11 @@ func register_platform(platform: String, x: float, y: float):
 
 func update_platforms(delta):
 	for platform in _platsforms:
-		platform.global_position.x += _speed * delta
+		platform.global_position += _speed * delta * _velocity
+
+func move(x: int, y: int):
+	if (abs(x) != 1 && x != 0) || (abs(y) != 1 && y != 0):
+		push_error("x and y must be 1 -1 or 1")
+		return
+	
+	_velocity = Vector2(x, y)
