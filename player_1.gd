d0b3081468c22bd9eea8f6c3aa20d19a3374245b
Certified-Racist <159023946+Certified-Racist@users.noreply.github.com>
2024-02-08 09:02:14 +0100

    g

--- player_1.gd
+++ player_1.gd
@@ -174,6 +174,8 @@ func _ready():
 
 func _main_ready():
 	game.register_platform("res://platforms/smb1-1.tscn", 0.01)
+	game.register_platform("res://platforms/dirt8-1.tscn", 1)
+	game.register_platform("res://platforms/dirt7-1.tscn", 1)
 	game.register_platform("res://platforms/dirt6-1.tscn", 1)
 	game.register_platform("res://platforms/dirt5-1.tscn", 1)
 	game.register_platform("res://platforms/dirt4-1.tscn", 1)
